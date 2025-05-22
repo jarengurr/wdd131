cbfa08fbd8fedd867a143d1ded08242fff547428
jarengurr <jarenwheat@gmail.com>
2025-05-22 15:50:46 -0700

    update bom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,56 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.querySelector("#favchap");
+  const button = document.querySelector("button");
+  const list = document.querySelector("ul");
+
+
+
+  //********************************************************* */
+
+
+
+  button.addEventListener('click', function () {
+
+
+    if (input.value.trim() !== '') {
+
+      const li = document.createElement("li");
+      const deleteButton = document.createElement("button");
+      li.textContent = input.value;
+      deleteButton.textContent = "❌";
+
+
+
+      li.append(deleteButton);
+      list.append(li);
+
+
+      input.value = "";
+      input.focus();
+
+      deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.value = '';
+        input.focus();
+      });
+
+
+    }
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
