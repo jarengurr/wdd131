0ce98cae0c194480312c919cb5d538dbaee9535d
jarengurr <jarenwheat@gmail.com>
2025-05-26 13:00:31 -0700

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,13 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 //let oLastModif = new Date(document.lastModified)
 let lmDate = document.lastModified
 document.getElementById("lastModified").innerHTML = lmDate;
+
+
+
+const hamburg = document.querySelector(".hmlink");
+const screenmenu = document.querySelector(".links");
+
+hamburg.addEventListener("click", () => {
+    hamburg.classList.toggle("active");
+    screenmenu.classList.toggle("active");
+});
