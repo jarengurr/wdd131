9c9741f5c7361ea2c1363f8f40f843d3a3d8e901
jarengurr <jarenwheat@gmail.com>
2026-01-17 12:40:52 -0800

    major update

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+//alert(document.lastModified);
+// returns: Tuesday, December 16, 2017 11:09:42
+//let oLastModif = new Date(document.lastModified)
+let lmDate = document.lastModified
+document.getElementById("lastModified").innerHTML = lmDate;
+// menu setup
+const mybutton = document.querySelector("#menu");
+const mynavigation = document.querySelector(".navigate")
+
+mybutton.addEventListener("click", () => {
+    mynavigation.classList.toggle("show");
+    mybutton.classList.toggle("show");
+});
